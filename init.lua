97c4cac82d2ea6106a80d66ce1c28ebcad86e1b1
Georgii Ustinov <ustinovgo@gmail.com>
2024-04-27 14:28:45 +0300

    Initial commit

---
+++ init.lua
@@ -0,0 +1,103 @@
+require('config')
+require('plugins')
+require('keymaps')
+
+
+local configs = require'nvim-treesitter.configs'
+configs.setup {
+ensure_installed = { "cpp", "rust", "kotlin", "java", "c", "lua", "vim", "vimdoc", "query" },
+highlight = { -- enable highlighting
+  enable = true,
+},
+indent = {
+  enable = true, -- default is disabled anyways
+}
+}
+
+
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+  local opts = {}
+  if server.name == "sumneko_lua" then
+    opts = {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'use' }
+          },
+          --workspace = {
+            -- Make the server aware of Neovim runtime files
+            --library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
+          --}
+        }
+      }
+    }
+  end
+  server:setup(opts)
+end)
+
+
+local function foo()
+  print "hello world!"
+end
+
+
+foo()
+
+local cmp = require'cmp'
+
+ cmp.setup({
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<c-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<c-f>'] = cmp.mapping.scroll_docs(4),
+      ['<c-Space>'] = cmp.mapping.complete(),
+      ['<c-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+  })
+
+  -- Set up lspconfig.
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  require('lspconfig')['sumneko_lua'].setup {
+    capabilities = capabilities
+  }
---
+++ lua/config.lua
@@ -0,0 +1,8 @@
+local o = vim.o
+local bo = vim.bo
+
+o.number = true
+vim.opt.shiftwidth = 2  -- Number of spaces to use for each step of (auto)indent
+vim.opt.tabstop = 2     -- Number of spaces that a <Tab> in the file counts for
+vim.opt.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.opt.expandtab = true -- Use spaces instead of tabs
