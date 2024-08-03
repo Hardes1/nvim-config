b6c44adb90c451f036cf3d7e62bae798811524e0
Georgii Ustinov <ustinovgo@gmail.com>
2024-08-03 19:25:22 +0300

    Use the same clipboard buffer for laptop and vim

--- lua/config.lua
+++ lua/config.lua
@@ -9,4 +9,6 @@ vim.opt.tabstop = 2     -- Number of spaces that a <Tab> in the file counts for
 vim.opt.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations
 vim.opt.expandtab = true -- Use spaces instead of tabs
 
+vim.api.nvim_set_option("clipboard", "unnamed")
+
 vim.cmd "colorscheme catppuccin"
