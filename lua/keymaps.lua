e73c9e7f0e948358e674553904d0c2edb610fddc
Georgii Ustinov <ustinovgo@gmail.com>
2024-08-25 15:52:30 +0300

    Add description for custom mapping

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,10 +1,14 @@
 local keymap = vim.api.nvim_set_keymap
 
+
+-- saving docs
 keymap('n', '<c-s>', ':w<CR>', {})
 keymap('i', '<c-s>', ':w<CR>', {})
 keymap('n', '<c-s-q>', ':q!<CR>', {})
 keymap('n', '<c-q>', ':q<CR>', {})
 local opts = { noremap = true }
+
+-- switching windows
 keymap('n', '<c-j>', '<c-w>j', opts)
 keymap('n', '<c-h>', '<c-w>h', opts)
 keymap('n', '<c-k>', '<c-w>k', opts)
@@ -14,7 +18,10 @@ local function nkeymap(hotkey, cmd)
     keymap('n', hotkey, cmd, opts)
 end
 
+
+-- enable nerd tree
 nkeymap('<c-y>', ':NERDTreeToggle<CR>')
 
+-- switch terminal
 nkeymap('<c-t>', ':ToggleTerm<CR>')
 
