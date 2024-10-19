975b7e1024c9f973e472a00f43e8bb03e8352580
Georgii <ustinovgo@gmail.com>
2024-10-19 13:34:51 +0000

    Fix snippet engine error

--- lua/hardes1/lsp.lua
+++ lua/hardes1/lsp.lua
@@ -19,6 +19,7 @@ local lspconfig = require('lspconfig')
 
 
 
+
 -- Lua LSP setup
 lspconfig.lua_ls.setup {
   on_attach = require('hardes1.lsp_bindings'),
@@ -47,14 +48,46 @@ lspconfig.lua_ls.setup {
   },
 }
 
+
+
 local cmp = require('cmp')
+local luasnip = require('luasnip')
 cmp.setup({
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+        end,
+  },
   mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected item.
+      ['<CR>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            if luasnip.expandable() then
+                luasnip.expand()
+            else
+                cmp.confirm({
+                    select = true,
+                })
+            end
+        else
+            fallback()
+        end
+     end),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+      if cmp.visible() then
+        local entry = cmp.get_selected_entry()
+        if not entry then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        end
+        cmp.confirm()
+      else
+        fallback()
+      end
+    end, {"i","s","c",})
     }),
     sources = {
         { name = 'nvim_lsp' },
